56240d35da8cbafcc987443922f58ac62ed7861a
squashy-v <jjamesvandergriff@gmail.com>
2025-01-27 11:22:01 -0500

    added closefloatingwindows function to handle the lazy window hanging

---
+++ lua/functions.lua
@@ -0,0 +1,8 @@
+function CloseFloatingWindows()
+    for _, win in pairs(vim.api.nvim_list_wins()) do
+        local config = vim.api.nvim_win_get_config(win)
+        if config.relative ~= "" then
+            vim.api.nvim_win_close(win, false)
+        end
+    end
+end
