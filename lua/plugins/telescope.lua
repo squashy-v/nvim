90ac17a263743d781c6d347b19cf461eb10ae617
squashy-v <jjamesvandergriff@gmail.com>
2025-01-29 01:28:11 -0500

    remapped telescope to allow mass adding to qf list on C-q instead of M-q

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,21 @@ return {
 
 	dependencies = { 'nvim-lua/plenary.nvim' },
 
+	config = function ()
+		require("telescope").setup({
+			defaults = {
+				mappings = {
+					n = {
+						["<M-q>"] = false,
+						["<C-q>"] = "send_selected_to_qflist",
+					}
+				}
+			}
+		})
+	end,
+
+
+
 	branch = '0.1.x',
 
 }
