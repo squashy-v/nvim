c0574a61b50fcbb878f1d00abc5b55986f4208c8
squashy-v <jjamesvandergriff@gmail.com>
2025-01-30 09:55:21 -0500

    autocmd would not recognize buff due to comparing table instead of string

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,7 +1,8 @@
 vim.api.nvim_create_autocmd({ "BufLeave" }, {
   pattern = "{}",
   callback = function()
-    if vim.api.nvim_buf_line_count(0) == 1 and vim.api.nvim_buf_get_lines(0, 0, 1, false) == "" then
+    -- print("cmd triggered: " .. os.date("%Y-%m-%d %H:%M:%S"))
+    if vim.api.nvim_buf_line_count(0) == 1 and vim.api.nvim_buf_get_lines(0, 0, 1, false)[1] == "" then
       vim.bo.buftype = "nofile"
       vim.bo.bufhidden = "wipe"
     end
