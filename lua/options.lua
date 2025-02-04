1e00abdb802f463b0e4cb842aeec82be6524e484
squashy-v <jjamesvandergriff@gmail.com>
2025-02-04 16:40:05 -0500

    added options suggested in kickstart.nvim

--- lua/options.lua
+++ lua/options.lua
@@ -1,32 +1,38 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.termguicolors = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
+vim.opt.breakindent = true
+vim.opt.colorcolumn = "80"
 
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = "yes"
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.opt.isfname:append("@-@")
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "80"
-
 vim.opt.conceallevel = 1
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.mouse = 'a'
+vim.opt.cursorline = true
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
