618374e0333af8defd1b159c2c052c7ee1d76da1
squashy-v <jjamesvandergriff@gmail.com>
2025-03-13 15:30:33 -0400

    retooling lsp file for more consistency

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -36,54 +36,24 @@ lspconfig.lua_ls.setup {
     }
 }
 
-lspconfig.omnisharp.setup {
-    cmd = { "dotnet", "/usr/bin/omnisharp" },
-
-    settings = {
-      FormattingOptions = {
-        -- Enables support for reading code style, naming convention and analyzer
-        -- settings from .editorconfig.
-        EnableEditorConfigSupport = true,
-        -- Specifies whether 'using' directives should be grouped and sorted during
-        -- document formatting.
-        OrganizeImports = nil,
-      },
-      MsBuild = {
-        -- If true, MSBuild project system will only load projects for files that
-        -- were opened in the editor. This setting is useful for big C# codebases
-        -- and allows for faster initialization of code navigation features only
-        -- for projects that are relevant to code that is being edited. With this
-        -- setting enabled OmniSharp may load fewer projects and may thus display
-        -- incomplete reference lists for symbols.
-        LoadProjectsOnDemand = nil,
-      },
-      RoslynExtensionsOptions = {
-        -- Enables support for roslyn analyzers, code fixes and rulesets.
-        EnableAnalyzersSupport = nil,
-        -- Enables support for showing unimported types and unimported extension
-        -- methods in completion lists. When committed, the appropriate using
-        -- directive will be added at the top of the current file. This option can
-        -- have a negative impact on initial completion responsiveness,
-        -- particularly for the first few completion sessions after opening a
-        -- solution.
-        EnableImportCompletion = nil,
-        -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-        -- true
-        AnalyzeOpenDocumentsOnly = nil,
-      },
-      Sdk = {
-        -- Specifies whether to include preview versions of the .NET SDK when
-        -- determining which version to use for project loading.
-        IncludePrereleases = true,
-      },
-    },
-}
-
 lspconfig.powershell_es.setup{
-  cmd = {'pwsh', '-NoLogo', '-NoProfile', '-Command', "/opt/powershell-editor-services/PowerShellEditorServices/Start-EditorServices.ps1 ..."}
+    cmd = {'pwsh', '-NoLogo', '-NoProfile', '-Command', "/opt/powershell-editor-services/PowerShellEditorServices/Start-EditorServices.ps1 ..."}
 }
--- temp removed from servers 'grammarly', 
-local servers = { 'clangd', 'gleam', 'hls', 'gopls', 'jdtls', 'kotlin_language_server', 'ts_ls', 'html', 'cssls', 'htmx', 'jsonls', 'markdown_oxide', 'nimls', 'perlnavigator', 'intelephense', 'pyright', 'bashls', 'solargraph', 'rust_analyzer', 'zls', 'teal_ls' }
+
+lspconfig.omnisharp.setup({
+    cmd = {
+        "/usr/bin/omnisharp", -- full path to binary
+        "-lsp",
+        "--hostPID",
+        tostring(vim.fn.getpid()),
+    },
+    enable_import_completion = true,
+    organize_imports_on_format = true,
+    capabilities = capabilities,
+})
+
+local servers = { 'clangd', 'gleam', 'gopls', 'jdtls', 'kotlin_language_server', 'ts_ls', 'html', 'cssls', 'jsonls', 'markdown_oxide', 'nimls', 'perlnavigator', 'intelephense', 'pyright', 'bashls', 'solargraph', 'rust_analyzer', 'zls', 'teal_ls' }
+-- manual updating hls teal_ls roslyn
 
 for _, server in ipairs(servers) do
     lspconfig[server].setup({
